63479ad54022392505cdecaf85f58f5a0293c9db
anyquestions69 <juuzodes@yandex.ru>
2025-03-26 18:26:38 +0300

    upd

--- panos_scripts/main.py
+++
@@ -1,4 +0,0 @@
-from panos.firewall import Firewall
-
-fw = Firewall("192.168.1.1", "admin", "admin")
-print(fw.show_system_info())
--- src/index.php
+++ src/index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
 <form action="/index.php">
 <input type="text" name="cmd" id="">    
     <button type="submit">Ping!</button>
@@ -8,3 +17,6 @@ if(isset($_GET['cmd'])){
    echo system(`ls | {$_GET['cmd']}`);
 }
 ?>
+<script src="index.js"></script>
+</body>
+</html>
